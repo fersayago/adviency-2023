850afaa2f9b6d4e6c5c77130ef22ba1e2ed6df35
fersayago <sayago.fernando@gmail.com>
2023-12-15 18:13:34 -0300

    Dia2: Estilos iniciales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Regalos:</h1>
-      <p>Medias</p>
-      <p>Caramelos</p>
-      <p>Vitel Tone</p>
+    <main>
+      <div className='flex items-center justify-center h-screen bg-cover bg-center' style={{
+        backgroundImage: 'url(/christmas-bg.png)'
+      }}>
+        <div className='bg-gray-100 p-6 rounded-lg text-black '>
+          <h1 className="text-red-500 font-bold top pb-2 text-xl">Regalos</h1>
+          <p>Medias</p>
+          <p>Caramelos</p>
+          <p>Vitel Tone</p>
+        </div>
+      </div>
     </main>
   )
 }
