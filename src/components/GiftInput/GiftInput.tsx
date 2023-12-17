550b982f0bcc04e972710dc3bad4c934bd7ad97e
fersayago <sayago.fernando@gmail.com>
2023-12-17 18:58:41 -0300

    feat: :sparkles: Solo se puede agregar regalo si no existe

--- src/components/GiftInput/GiftInput.tsx
+++ src/components/GiftInput/GiftInput.tsx
@@ -5,18 +5,31 @@ import { GiftsContext } from '@/components/GiftsProvider'
 
 const GiftInput = () => {
   const [value, setValue] = React.useState<string>('')
-  const { addGift } = React.useContext(GiftsContext)
+  const [ isRepeated, setIsRepeated ] = React.useState<boolean>(false)
+  const { gifts, addGift } = React.useContext(GiftsContext)
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-
     e.preventDefault()
+
+    const isGiftRepeated = gifts.find(gift => gift.name === value)
+
+    if (isGiftRepeated) {
+      setIsRepeated(true);
+      return
+    }
+
     addGift({
       id: crypto.randomUUID(),
       name: value
     })
     
     setValue('')
+    setIsRepeated(false);
   }
+  
+  const buttonClassName = value.length === 0
+    ? 'bg-red-600 border border-black text-white px-4 py-2 rounded-r-lg hover:bg-red-500 transition duration-300 ease-in-out opacity-50 cursor-not-allowed'
+    : 'bg-red-600 border border-black text-white px-4 py-2 rounded-r-lg hover:bg-red-500 transition duration-300 ease-in-out'
 
   return (
     <form onSubmit={handleSubmit}>
@@ -29,11 +42,19 @@ const GiftInput = () => {
       />
 
       <button
-        className='bg-red-600 border border-black text-white px-4 py-2 rounded-r-lg hover:bg-red-500 transition duration-300 ease-in-out'
+        className={buttonClassName}
         type='submit'
+        disabled={value.length === 0}
       >
         Agregar
       </button>
+      {
+        isRepeated && (
+          <p className='text-red-600 text-xs mt-1 ml-2'>
+            El regalo ya existe
+          </p>
+        )
+      }
     </form>
   )
 }
